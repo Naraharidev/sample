d2ff2acb8fcdd4c7d363fbe0b5f0d32746d73c85
Narahari <naraharidevarapu@gmail.com>
2022-10-18 23:42:58 +0530

    FIRST

---
+++ OOP/src/Inheritance/First.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated 
+		System.out.println("Hello world!");
+		for(int i=0;i<10;i++) System.out.println(i);
+	}
+
+}
